5f16683a5db013c2003b2e6c371230eefeeb7332
karanaulakh <karansaulakh@gmail.com>
2025-11-23 09:58:18 -0700

    linter error

--- frontend/src/utils/apiService.js
+++ frontend/src/utils/apiService.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 // Create an instance of axios with the base URL
-const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL =
+  process.env.VUE_APP_API_BASE_URL || "http://localhost:5001";
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
